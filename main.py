4d933d987610dc1583897108c6f9425c6e3624e6
Florian Kempenich <ShockN745@gmail.com>
2016-02-03 21:20:29 +0100

    Group setup and run tests in a single test. Add tearDown test

--- main.py
+++ main.py
@@ -6,40 +6,37 @@ class TestCase:
     def set_up(self):
         pass
 
+    def tear_down(self):
+        pass
+
     def run(self):
         self.set_up()
         method = getattr(self, self.name)
         method()
+        self.tear_down()
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
-        self.wasSetUp = None
 
     def set_up(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
+
+    def tear_down(self):
+        self.log = self.log + 'tearDown'
 
     def test_method(self):
-        self.wasRun = 1
+        self.log = self.log + "test_method "
 
 
 class TestCaseTest(TestCase):
 
-    def set_up(self):
+    def test_template_method(self):
         self.test = WasRun('test_method')
-
-    def test_running(self):
         self.test.run()
-        assert self.test.wasRun
-
-    def test_set_up(self):
-        self.test.run()
-        assert self.test.wasSetUp
-
+        assert self.test.log == "setUp test_method tearDown"
 
 
 if __name__ == '__main__':
-    TestCaseTest("test_running").run()
-    TestCaseTest("test_set_up").run()
+    TestCaseTest("test_template_method").run()
